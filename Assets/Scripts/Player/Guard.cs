5d253bcc132001c26e3f1273ef24ba42bd8dfb2a
GermanScientist <jack_hulspas@hotmail.com>
2023-02-13 16:36:04 +0100

    Added methods to gain and lose currency

--- Assets/Scripts/Player/Guard.cs
+++ Assets/Scripts/Player/Guard.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class Guard : Player {
     [Header("Guard stats")]
     [SerializeField] private float currency = 10;
+
+    //Set the guard's currency
+    public void SetCurrency(int _value) {
+        currency = _value;
+    }
+
+    //Make the guard lose currency
+    public void LoseCurrency(int _value) {
+        currency -= _value;
+    }
+
+    //Make the guard gain currency
+    public void AddCurrency(int _value) {
+        currency += _value;
+    }
 }
