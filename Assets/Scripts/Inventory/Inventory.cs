325c3bdb2c70ea4612a3f4c793e4f8458796c3f6
GermanScientist <jack_hulspas@hotmail.com>
2023-03-24 22:34:13 +0100

    Added inventory functionality

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -2,5 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour {
+public class Inventory {
+    private List<Item> items;
+    private System.Type selectedItem;
+
+    public void SelectItem(System.Type _type) {
+        selectedItem = _type;
+    }
+
+    public void AddItem(Item _item) {
+        if (_item == null) return;
+        items.Add(_item);
+    }
+
+    public void RemoveItem(Item _item) {
+        if (_item == null) return;
+        items.Remove(_item);
+    }
+
+    public int CheckItemAmount(System.Type _type) {
+        int amount = 0;
+        foreach (Item _item in items) {
+            if (_item.GetType() == _type) amount++;
+        }
+
+        return amount;
+    }
 }
